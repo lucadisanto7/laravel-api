d8451f092f20c07dd69d04f02a6cc49b2665b837
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-17 09:53:15 +0200

    [UPD] Added  controlled to api.php and changed controller name from post to project

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
 
-class PostController extends Controller
+class ProjectController extends Controller
 {
     public function index(){
         $projects = Project::all();
