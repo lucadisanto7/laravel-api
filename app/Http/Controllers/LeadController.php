ade8570c8c01dc81b80655c90b8bdd64676bfee4
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-22 23:49:52 +0200

    [UPD] Imported validator

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Lead;
 
 class LeadController extends Controller
