eb7630c8c0f0e73c0a27e61dc23b5c11a81ef61b
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-23 00:02:23 +0200

    [UPD] Added content and style to contact email

---
+++ resources/views/emails/contact_email.blade.php
@@ -0,0 +1,9 @@
+<div style="width: 90%; margin: 0 auto;">
+    <header style="background-color: black; color: white">
+        <h1 style="text-align: center">Boolfolio</h1>
+    </header>
+    <main>
+        <h2>Nuovo contatto ricevuto dalla form del sito</h2>
+        <p>Ecco i dati:</p>
+    </main>
+</div>
