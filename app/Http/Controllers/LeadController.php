06a5571aad1a8c61e9ce7e71690364505109b339
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-22 23:49:25 +0200

    [UPD] Imported controller and created store function

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Lead;
 
 class LeadController extends Controller
 {
-    //
+    public function store( Request $request){
+        $form_data = $request->all();
+    }
 }
