b92570cf2802ea00497cb92f46bd5522f45a4070
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-22 23:54:38 +0200

    [UPD] Added validator and errors

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -11,5 +11,26 @@ class LeadController extends Controller
 {
     public function store( Request $request){
         $form_data = $request->all();
+
+        $validator = Validator::make($form_data,[
+            'name' => ['required', 'max:50'],
+            'surname' => ['required', 'max:100'],
+            'email' => ['required', 'max:150'],
+            'phone' => ['required', 'max:20'],
+            'content' => ['required'],
+        ],
+        $errors = [
+            'name.required' => 'Il nome e obbligatorio',
+            'name.max' => 'Il nome devve essere lungo al massimo :max caratteri',
+            'surname.required' => 'Il cognome e obbligatorio',
+            'surname.max' => 'Il cognome devve essere lungo al massimo :max caratteri',
+            'email.required' => 'L email e obbligatorio',
+            'email.max' => 'L email devve essere lungo al massimo :max caratteri',
+            'phone.required' => 'Il telefono e obbligatorio',
+            'phone.max' => 'Il telefono devve essere lungo al massimo :max caratteri',
+            'content.required' => 'Il contenuto della mail e obbligatorio',
+        ]
+        );
+        
     }
 }
